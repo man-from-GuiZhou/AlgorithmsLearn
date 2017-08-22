c87f5b4fb25c3f73d0310a54f30c4c73a5458c1f
yuhao <yuhao@YUHAO-PC>
2017-08-22 18:14:05 +0800

    打卡

---
+++ src/com/learn/chapterOne/BooleanList.java
@@ -0,0 +1,51 @@
+package com.learn.chapterOne;
+
+public class BooleanList {
+	boolean[][] bList=new boolean[5][5];
+	int randomNum=0;
+	
+	public BooleanList(){
+		this.initList();
+	}
+	
+	private void initList(){
+	for(int i=0;i<bList.length;i++){
+		for(int j=0;j<bList[0].length;j++){
+				randomNum=(int)(Math.random()*10);
+				//System.out.print(randomNum);
+				if(randomNum>=5){
+					bList[i][j]=true;
+				}
+			}
+		}
+	//return bList;
+	}
+	
+	public void printList(){
+		int row=0;
+		for(boolean[] tempL:bList){
+			if(row==0){
+				for(int i=0;i<tempL.length+1;i++){
+					if(i!=0){
+						System.out.print(i+" ");
+					}
+					else{
+						System.out.print("  ");
+					}
+				}
+			}
+			System.out.println();
+			row++;
+			System.out.print(row+" ");
+			for(boolean tempE:tempL){
+					//randomNum=(int)(Math.random()*10);
+					if(tempE==true){
+						System.out.print("* ");
+					}
+					else{
+						System.out.print("  ");
+					}
+				}
+			}
+		}
+}
---
+++ src/com/learn/main/MainProgram.java
@@ -0,0 +1,12 @@
+package com.learn.main;
+
+import com.learn.chapterOne.BooleanList;
+
+public class MainProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new BooleanList().printList();
+	}
+
+}
